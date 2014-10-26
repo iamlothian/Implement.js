a2804a4cb76ade5f25d1e3adff02ad0d6fc2d29c
Matthew Lothian <matthew.lothian@iit-laptop-3.industrieit.local>
2014-10-27 10:51:43 +1100

    moved examples to folder

---
+++ examples/bits.js
@@ -0,0 +1,16 @@
+
+var _final = function(value) {
+	// make sure we can't call this as a function, and that this 
+	// is always an object
+	if (!(( !! this && this.__safe__) || this instanceof _private != 0))
+		throw new Error();
+
+	var _value = value;
+
+	// keep a local reference to this
+	// so it doesn't get lost in extension
+	var self = this;
+
+};
+
+
