a0e03d32bfac37fecc965b99abfc2c859d1b500f
iamlothian <lothian.studios@gmail.com>
2015-04-04 01:44:00 +1100

    added a better example creating a custome element and some other bits

--- examples/bits.js
+++ examples/bits.js
@@ -1,56 +1,95 @@
 
-// #################
-// using prototypes
-// #################
+// implements a new custome element
+var iElement = (function(){
 
-var x = 1;
-var xx = x;
+	var tagName = "i_element";
 
-var A = function A() {
-  
-  var _self = this;
-  var _proto = this.constructor.prototype;
-  _self.name = "A";
- 
-  _proto.getName = function(){
-    return this.name;
-  };
-  
-};
+	return function iElement () {
 
-var B = function B() {
-  
-  var _self = this;
-  var _proto = this.constructor.prototype;
-  _self.name = "B";
- 
-  _proto.Speek = function(){
-    return "Hello" + this.name;
-  };
+		// make sure we can't call this as a function, and that this 
+		// is always an object
+		if (!(( !! this && this.__safe__) || this instanceof iElement != 0))
+			throw new Error("Incorrect use of instance constructor function");
 
-  var x = 1;
+		if (!document && !document.createElement)
+			throw new Error("No document object found");
 
-  _proto.age = function(){
-    return x;
-  };
-  
-};
+		var self = this;
+		var proto = this.constructor.prototype;
 
-var A1 = new A();
-var A2 = A.Implement();
-var A3 = Function.Implement(A, B);
+		// make new element for each instance;
+		this.element = document.createElement(tagName);
 
-var B = A3.Extend(function(){
+	};
 
-  var _self = this;
-  var _proto = this.constructor.prototype;
-  _self.name = "BB";
+})();
 
-  var x = 2;
+// addeds box styles to an element
+var BoxStyles = (function(){
 
-})
+	return function BoxStyles (w, h, color, margin) {
 
-console.log(A1, A1.getName());
-console.log(A2, A2.getName());
-console.log(A3, A3.getName());
-console.log(B, B.getName());
+		// make sure we can't call this as a function, and that this 
+		// is always an object
+		if (!(( !! this && this.__safe__) || this instanceof BoxStyles != 0))
+			throw new Error("Incorrect use of instance constructor function");
+
+		if(!this.__isInstanceOf__(iElement))
+			throw new Error("Expected ancestor to be iElement instance");
+
+		var self = this;
+		var proto = this.constructor.prototype;
+
+		this.element.style.display = "block";
+		this.element.style.width = w || 0;
+		this.element.style.height = h || 0;
+		this.element.style.backgroundColor = color || "black";
+		this.element.style.margin = margin || 0;
+
+	};
+
+})();
+
+// adds click handler to element
+var Clickable = (function(){
+
+	return function Clickable (handler) {
+
+		// make sure we can't call this as a function, and that this 
+		// is always an object
+		if (!(( !! this && this.__safe__) || this instanceof BoxStyles != 0))
+			throw new Error("Incorrect use of instance constructor function");
+
+		if(!this.__isInstanceOf__(iElement))
+			throw new Error("Expected ancestor to be iElement instance");
+
+		var self = this;
+		var proto = this.constructor.prototype;
+
+		this.element.addEventListener('click', handler);
+
+	};
+
+})();
+
+var E1 = iElement.Implement(
+	[BoxStyles, 100, 100, "Red", 5]
+);
+
+var E2 = E1.Extend(
+	[Clickable, function(e){ alert("clicked"); }]
+);
+
+var E3 = iElement.Implement(
+	[BoxStyles, 200, 100, "green"],
+	[Clickable, function(e){ alert("clicked the green one"); }]
+);
+
+// add our new element to the dom
+window.onload = function(){
+
+	document.body.appendChild(E1.element);
+	document.body.appendChild(E2.element);
+	document.body.appendChild(E3.element);
+
+}
